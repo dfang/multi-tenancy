755224625d8c9112d0313690f05a9f5a70691a70
fang duan <df1228@gmail.com>
2019-09-10 17:15:05 +0800

    Update publish_to_ghpages.sh

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -7,22 +7,13 @@ then
 fi
 
 echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
-
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
-
-echo "Removing existing files"
-rm -rf public/*
+rm -rf docs/*
 
 echo "Generating site"
-hugo --baseURL=/multi-tenancy/
+hugo --baseURL=/multi-tenancy/ -d docs/
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+echo "Updating gh-pages"
+git add --all && git commit -m "Publishing to github pages"
 
 echo "Pushing to github"
 git push --all
